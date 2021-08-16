dbefe225b185e4d3d881c0b903d04e28f510c0ee
zhongyao603296 <zhongyao603296@pwrd.com>
2021-08-16 17:47:54 +0800

    创建音频Activity

--- app/src/main/java/com/hongri/multimedia/MainActivity.java
+++ app/src/main/java/com/hongri/multimedia/MainActivity.java
@@ -2,13 +2,38 @@ package com.hongri.multimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.hongri.multimedia.audio.AudioActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button audioBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        audioBtn = findViewById(R.id.audioBtn);
+        audioBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        Intent intent;
+        switch (id) {
+            case R.id.audioBtn:
+                intent = new Intent(MainActivity.this, AudioActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/hongri/multimedia/audio/AudioActivity.java
@@ -0,0 +1,18 @@
+package com.hongri.multimedia.audio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hongri.multimedia.R;
+
+public class AudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+
+
+    }
+}
